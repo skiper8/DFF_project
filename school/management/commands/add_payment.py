3cca20ab06fdce1b6e856ec1301b43122a00feae
askip <a.skiper2013@yandex.ru>
2023-07-31 17:20:38 +0300

    Исправление багов и добавление .env.sample

--- school/management/commands/add_payment.py
+++ school/management/commands/add_payment.py
@@ -1,16 +1,23 @@
 from django.core.management import BaseCommand
 
-from school.models import Payment
+from school.models import Course, Payment
+from users.models import User
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        User.objects.all().delete()
+        Course.objects.all().delete()
         Payment.objects.all().delete()
 
+        Course.objects.create(title="Django", price=10)
+        User.objects.create(email='test@test.com', is_active=True)
+
         payment_list = [
-            {'course': 'Django', 'payment_method': 'наличными средствами'},
-            {'user': 3, 'course': 11, 'payment_method': 'наличными средствами'},
+            {'course': Course.objects.get(title='Django'), 'payment_method': 'наличными средствами'},
+            {'user': User.objects.get(email='test@test.com'), 'course': Course.objects.get(title='Django'),
+             'payment_method': 'наличными средствами'},
         ]
 
         payment_objects = []
