d781a074835f0ea18e3886eced68bcfa112cf7ba
askip <a.skiper2013@yandex.ru>
2023-08-24 22:39:19 +0300

    удаление файла .env.docker

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ WORKDIR /app
 
 COPY requirements.txt /app/
 
+RUN pip install --upgrade pip
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
-
-CMD ["python", "manage.py", "runserver"]
--- config/settings.py
+++ config/settings.py
@@ -96,10 +96,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME_DB'),
-        'USER': os.getenv('USER_DB'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST')
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
