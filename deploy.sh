95d94cb8bd4482ad3887e1af78376c8d730f065a
askip <a.skiper2013@yandex.ru>
2023-08-29 20:19:31 +0300

    исправление файла .gitlab-ci.yml

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-python3 -m venv venv
-source  venv/bin/activate
+pip3 install virtualenv
+virtualenv venv
+source venv/bin/activate
 pip3 install -r requirement.txt
 python3 manage.py migrate
