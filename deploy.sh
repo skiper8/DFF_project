4207840e02f2b19754ffe6bd00749a4fd5d81356
askip <a.skiper2013@yandex.ru>
2023-09-01 00:35:36 +0300

    изменение .gitlab-ci.yml

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py migrate
-python3 manage.py test
 coverage run --source='.' manage.py test
 coverage report
 deactivate
