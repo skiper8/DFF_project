40cc565c4792d72ba5290b7df42f0f3178aefa71
askip <a.skiper2013@yandex.ru>
2023-08-31 21:04:28 +0300

    изменение .gitlab-ci.yml

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 command = '/var/www/html/drf_project/venv/bin/gunicorn'
 pythonpath = '/var/www/html/drf_project'
-bind = '84.201.161.160'
+bind = '84.201.161.160:8000'
 workers = 3
 user = 'skiper88'
