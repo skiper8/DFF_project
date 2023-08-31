130243df3060a5aed734d54a0ad375b8e0592bad
askip <a.skiper2013@yandex.ru>
2023-09-01 00:42:22 +0300

    изменение .gitlab-ci.yml

--- config/settings.py
+++ config/settings.py
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'NAME': 'drf_db',
+        'USER': 'skiper88',
+        'PASSWORD': 'yourpassword',
         'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT'),
     }
