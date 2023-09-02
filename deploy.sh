664d9949c88fa8edbea83d0f0669c0d9385d471a
askip <a.skiper2013@yandex.ru>
2023-09-02 16:15:41 +0300

    изменение в deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py csu
-python3 manage.py test
 coverage run --source='.' manage.py test
 coverage report
 deactivate
